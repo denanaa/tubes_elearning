cd1173b9879b0c42d7afd4b6c3fd5779cbf6f705
Davinaputku <davinaputri2408@gmail.com>
2024-12-18 10:44:02 +0700

    menambahkan fitur profile

--- routes/web.php
+++ routes/web.php
@@ -4,12 +4,16 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    return view('welcome'); 
+})->name('welcome');
+
+Route::get('/welcome', function () {
     return view('welcome');
-});
+})->middleware(['auth', 'verified'])->name('welcome');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+})->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
