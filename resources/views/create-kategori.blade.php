1f2f3e5592669886a230e2c590f08f91b2048d7f
AldoLeonard <albaihaqialdo99@gmail.com>
2024-12-29 23:53:45 +0700

    Merubah view data category

--- resources/views/create-kategori.blade.php
+++ resources/views/create-kategori.blade.php
@@ -1,26 +1,25 @@
 @extends('layouts.app')
 
-@section('title', 'Add Data Kategori')
-@section('header', 'Add Data Kategori')
+@section('title', 'Add Data Category')
+@section('header', 'Add Data Category')
 
 @section('content')
 <div class="container">
     <form>
-        <div class="my-4 space-y-1">
-            <label for="editName" class="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">Name</label>
-            <input type="text" id="editName" name="name" class="flex w-full max-w-xs h-10 px-3 py-2 text-sm bg-white border rounded-md peer border-neutral-300 ring-offset-background placeholder:text-neutral-400 focus:border-neutral-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-neutral-400 disabled:cursor-not-allowed disabled:opacity-50" placeholder="name">
-        </div>       
-        <div class="my-4 space-y-1">
-            <label for="editEmail" class="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">Email</label>
-            <input type="email" id="editEmail" name="email" class="flex w-full max-w-xs h-10 px-3 py-2 text-sm bg-white border rounded-md peer border-neutral-300 ring-offset-background placeholder:text-neutral-400 focus:border-neutral-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-neutral-400 disabled:cursor-not-allowed disabled:opacity-50" placeholder="user@gmail.com">
-        </div>
-        <div class="my-4 space-y-1">
-            <label for="editRole" class="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">Role</label>
-            <select id="editRole" name="role" class="flex w-full max-w-xs h-10 px-3 py-2 text-sm bg-white border rounded-md peer border-neutral-300 ring-offset-background placeholder:text-neutral-400 focus:border-neutral-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-neutral-400 disabled:cursor-not-allowed disabled:opacity-50">
-                <option value="Admin">Admin</option>
-                <option value="User">User</option>
-            </select>
-        </div>
+            <div class="my-4 space-y-1">
+                <label for="editImage" class="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">Image Category</label>
+                <input type="file" id="editImage" name="image" class="flex w-full h-10 px-3 py-2 text-sm bg-white border rounded-md peer border-neutral-300 ring-offset-background placeholder:text-neutral-400 focus:border-neutral-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-neutral-400 disabled:cursor-not-allowed disabled:opacity-50" placeholder="Image Category" accept="images/*"
+        onchange="previewImage(event)">
+                <img id="imagePreview" src="" alt="Image Preview" class="w-32 h-32 mt-2 object-cover rounded-lg hidden">
+            </div>
+            <div class="my-4 space-y-1">
+                <label for="editName" class="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">Name Category</label>
+                <input type="text" id="editName" name="name" class="flex w-full h-10 px-3 py-2 text-sm bg-white border rounded-md peer border-neutral-300 ring-offset-background placeholder:text-neutral-400 focus:border-neutral-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-neutral-400 disabled:cursor-not-allowed disabled:opacity-50" placeholder="Name Category">
+            </div>
+            <div class="my-4 space-y-1">
+                <label for="editDescription" class="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">Description Category</label>
+                <textarea type="text" id="editDescription" name="description" rows ="6" class="flex w-full px-3 py-2 text-sm bg-white border rounded-md peer border-neutral-300 ring-offset-background placeholder:text-neutral-400 focus:border-neutral-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-neutral-400 disabled:cursor-not-allowed disabled:opacity-50" placeholder="Description Category"></textarea>
+            </div>
         <div class="flex justify-start">
             <button type="submit" class="mt-3 inline-flex w-full justify-center rounded-md bg-blue-500  px-3 py-2 text-sm font-semibold text-white shadow-sm ring-gray-300 hover:bg-blue-600 sm:mt-0 sm:w-auto">Save</button>
             <a href="{{ route('data-kategori') }}">
@@ -29,4 +28,22 @@
         </div>
     </form>
 </div>
+
+<script>
+    function previewImage(event) {
+        const imagePreview = document.getElementById('imagePreview');
+        const file = event.target.files[0];
+        if (file) {
+            const reader = new FileReader();
+            reader.onload = () => {
+                imagePreview.src = reader.result;
+                imagePreview.classList.remove('hidden');
+            };
+            reader.readAsDataURL(file);
+        } else {
+            imagePreview.classList.add('hidden');
+        }
+    }
+</script>
+
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -45,7 +45,7 @@
                                 <a href="data-modul" class="flex items-center rounded-md gap-2 px-2 py-1.5 text-sm font-medium text-white underline-offset-2 hover:bg-white hover:text-[#37AFE1] focus-visible:underline focus:outline-none">Data Modul</a>
                             </li>
                             <li class="px-1 py-0.5 first:mt-2">
-                                <a href="data-kategori" class="flex items-center rounded-md gap-2 px-2 py-1.5 text-sm font-medium text-white underline-offset-2 hover:bg-white hover:text-[#37AFE1] focus-visible:underline focus:outline-none">Data Kategori</a>
+                                <a href="data-kategori" class="flex items-center rounded-md gap-2 px-2 py-1.5 text-sm font-medium text-white underline-offset-2 hover:bg-white hover:text-[#37AFE1] focus-visible:underline focus:outline-none">Data Category</a>
                             </li>
                             <li class="px-1 py-0.5 first:mt-2">
                                 <a href="data-video" class="flex items-center rounded-md gap-2 px-2 py-1.5 text-sm font-medium text-white underline-offset-2 hover:bg-white hover:text-[#37AFE1] focus-visible:underline focus:outline-none">Data Video</a>
